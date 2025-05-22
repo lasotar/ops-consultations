fb1e06b12c99dbb2967dbd2ed961dd7b4bcea6c5
lasotar <lasota.rafal.04@gmail.com>
2025-05-22 13:27:01 +0200

    client fixing

--- client_from_gpt.c
+++ client_from_gpt.c
@@ -1,89 +1,126 @@
 #include "common.h"
 
-#define MAX_MESSAGE_SIZE 64
-#define MAX_USERNAME_SIZE 32
-
-void usage(char *name)
-{
-    fprintf(stderr, "USAGE: %s [server_address] [port]\n", name);
-    exit(EXIT_FAILURE);
-}
+#define MAX_EVENTS 2
+#define BUF_SIZE 128
 
 int main(int argc, char **argv)
 {
     if (argc != 3)
-        usage(argv[0]);
+    {
+        fprintf(stderr, "Usage: %s server_ip server_port\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
     signal(SIGPIPE, SIG_IGN);
 
     int sockfd = connect_tcp_socket(argv[1], argv[2]);
-    int epoll_fd = epoll_create1(0);
-    if (epoll_fd < 0)
+
+    // Set socket non-blocking
+    int flags = fcntl(sockfd, F_GETFL, 0);
+    if (flags == -1)
+        ERR("fcntl F_GETFL");
+    if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1)
+        ERR("fcntl F_SETFL");
+
+    printf("Connected to server (fd = %d)\n", sockfd);
+
+    int epfd = epoll_create1(0);
+    if (epfd < 0)
         ERR("epoll_create");
 
-    struct epoll_event ev, events[2];
+    struct epoll_event ev, events[MAX_EVENTS];
 
-    // Add socket to epoll
     ev.events = EPOLLIN;
     ev.data.fd = sockfd;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sockfd, &ev) < 0)
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev) < 0)
         ERR("epoll_ctl sockfd");
 
-    // Add stdin to epoll
     ev.events = EPOLLIN;
     ev.data.fd = STDIN_FILENO;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, STDIN_FILENO, &ev) < 0)
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev) < 0)
         ERR("epoll_ctl stdin");
 
-    char buffer[MAX_MESSAGE_SIZE];
-    printf("Connected to server. Waiting for prompt...\n");
+    char buf[BUF_SIZE];
 
     while (1)
     {
-        int ready = epoll_wait(epoll_fd, events, 2, -1);
-        if (ready < 0)
+        int n = epoll_wait(epfd, events, MAX_EVENTS, -1);
+        if (n < 0)
             ERR("epoll_wait");
 
-        for (int i = 0; i < ready; i++)
+        for (int i = 0; i < n; ++i)
         {
-            if (events[i].data.fd == STDIN_FILENO)
-            {
-                // Read from user and send to server
-                memset(buffer, 0, sizeof(buffer));
-                ssize_t bytes = bulk_read(STDIN_FILENO, buffer, MAX_MESSAGE_SIZE - 1);
-                if (bytes <= 0)
-                {
-                    printf("Input closed.\n");
-                    goto cleanup;
-                }
+            int fd = events[i].data.fd;
 
-                if (bulk_write(sockfd, buffer, bytes) < 0)
+            if (fd == STDIN_FILENO)
+            {
+                // Read one line from stdin (blocking), send to server
+                ssize_t len = read(STDIN_FILENO, buf, BUF_SIZE - 1);
+                if (len > 0)
                 {
-                    fprintf(stderr, "Disconnected while writing.\n");
-                    goto cleanup;
+                    buf[len] = '\0';
+
+                    ssize_t sent = 0;
+                    while (sent < len)
+                    {
+                        ssize_t w = bulk_write(sockfd, buf + sent, len - sent);
+                        if (w < 0)
+                        {
+                            fprintf(stderr, "Server closed the connection\n");
+                            close(sockfd);
+                            exit(EXIT_FAILURE);
+                        }
+                        sent += w;
+                    }
+                }
+                else if (len == 0)
+                {
+                    fprintf(stderr, "EOF on stdin, exiting.\n");
+                    close(sockfd);
+                    exit(EXIT_SUCCESS);
+                }
+                else
+                {
+                    perror("read stdin");
+                    close(sockfd);
+                    exit(EXIT_FAILURE);
                 }
             }
-            else if (events[i].data.fd == sockfd)
+            else if (fd == sockfd)
             {
-                // Read from server and print
-                memset(buffer, 0, sizeof(buffer));
-                ssize_t bytes = bulk_read(sockfd, buffer, MAX_MESSAGE_SIZE - 1);
-                if (bytes <= 0)
+                // Read all available data from socket until EAGAIN
+                while (1)
                 {
-                    printf("Server closed connection.\n");
-                    goto cleanup;
+                    ssize_t len = read(sockfd, buf, BUF_SIZE - 1);
+                    if (len > 0)
+                    {
+                        buf[len] = '\0';
+                        printf("%s", buf);
+                        fflush(stdout);
+                    }
+                    else if (len == 0)
+                    {
+                        fprintf(stderr, "Disconnected from server.\n");
+                        close(sockfd);
+                        exit(EXIT_SUCCESS);
+                    }
+                    else
+                    {
+                        if (errno == EAGAIN || errno == EWOULDBLOCK)
+                        {
+                            // No more data right now
+                            break;
+                        }
+                        else
+                        {
+                            perror("read socket");
+                            close(sockfd);
+                            exit(EXIT_FAILURE);
+                        }
+                    }
                 }
-
-                set_color(STDOUT_FILENO, SOP_LIGHTGRAY);
-                bulk_write(STDOUT_FILENO, buffer, bytes);
-                reset_color(STDOUT_FILENO);
             }
         }
     }
-
-cleanup:
-    close(sockfd);
-    close(epoll_fd);
-    return 0;
 }
 
